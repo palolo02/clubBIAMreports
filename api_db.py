d810ee0e91a415d26f3a768f556dd1b8ae63301d
Paolo Vega <pao02esc@gmail.com>
2020-10-15 23:11:58 -0500

    Update in app

--- api_db.py
+++ api_db.py
@@ -15,115 +15,60 @@ import datetime
 import calendar
 
 # import variables from config file
-from credentials import host
-from credentials import pwd
-from credentials import usr
-from credentials import dialect
-from credentials import port
-from credentials import db
+from credentials import Config
+
+connection = Config.SQLALCHEMY_DATABASE_URI
 
 
-
-connection = f'{dialect}://{usr}:{pwd}@{host}:{port}/{db}'
-
-
-def getAllResults():
-    engine = create_engine(connection)
-
-    # reflect an existing database into a new model
-    Base = automap_base()
-    # reflect the tables
-    Base.prepare(engine, reflect=True)
-    # display tables/classes
-    display(Base.classes.keys())
-
-    # Save references to each table
-    Member = Base.classes.Member
-    Club = Base.classes.Club
-    Role = Base.classes.Role
-    Role_Type = Base.classes.Role_Type
-    Session = Base.classes.Session
-    Session_Type = Base.classes.Session_Type
-
-    # Start session
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-    session = Session()
-
-    # Read Tables to determine what task to perform in each
-    member_data = pd.read_sql_table("Member",engine)
-    club_data = pd.read_sql_table("Club",engine)
-    role_data = pd.read_sql_table("Role",engine)
-    role_type_data = pd.read_sql_table("Role_Type",engine)
-    session_data = pd.read_sql_table("Session",engine)
-    session_type_data = pd.read_sql_table("Session_Type",engine)
-    engine.dispose()
-
-    result = session.query(Session2,Member,Role,Role_Type, Club).join(Member,Session2.member_id==Member.member_id).join(Role,Session2.role_id==Role.role_id).join(Role_Type,Role.role_type_id==Role_Type.role_type_id).join(Club,Member.club_id==Club.club_id).all()
+def getAllResults(_member,_year,_engine):
+    rs = _engine.execute(f'Select  m.member_desc,  (Extract(Year from session_dt)) as Anio,  (Extract(Month from session_dt)) as Mes,  count(*) as NoPart  from public."Session" s  JOIN public."Member" m  ON m.member_id = s.member_id  where m.member_desc like {_member}  and (Extract(Year from session_dt)) = {_year}  and "isGuest" = false  group by (Extract(Year from session_dt)),  (Extract(Month from session_dt)),  m.member_desc ')
+    _engine.dispose()
     dicts = []
-    for row in result:
+    for row in rs:
         d = {}
-        print(row.Session.session_dt, row.Role.role_desc, row.Role.role_desc, row.Role_Type.role_type_desc, row.Club.club_desc)
-        d['session_dt'] = row.Session.session_dt
-        d['member_desc'] = row.Role.role_desc
-        d['role_desc'] = row.Role.role_desc
-        d['role_type_desc'] = row.Role_Type.role_type_desc
-        d['club_desc'] = row.Club.club_desc
+        d['member_desc'] = row[0]
+        d['year'] = row[1]
+        d['month'] = row[2]
+        d['nopart'] = row[3]
         dicts.append(d)
-    
-    return pd.DataFrame(dicts)
-
-def getResultsPerMember(member_):
-    engine = create_engine(connection)
-
-    # reflect an existing database into a new model
-    Base = automap_base()
-    # reflect the tables
-    Base.prepare(engine, reflect=True)
-    # display tables/classes
-    display(Base.classes.keys())
-
-    # Save references to each table
-    Member = Base.classes.Member
-    Club = Base.classes.Club
-    Role = Base.classes.Role
-    Role_Type = Base.classes.Role_Type
-    Session = Base.classes.Session
-    Session_Type = Base.classes.Session_Type
-
-    # Start session
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-    session = Session()
-
-    # Read Tables to determine what task to perform in each
-    member_data = pd.read_sql_table("Member",engine)
-    club_data = pd.read_sql_table("Club",engine)
-    role_data = pd.read_sql_table("Role",engine)
-    role_type_data = pd.read_sql_table("Role_Type",engine)
-    session_data = pd.read_sql_table("Session",engine)
-    session_type_data = pd.read_sql_table("Session_Type",engine)
-    engine.dispose()
-
-    result = session.query(Session2,Member,Role,Role_Type, Club).join(Member,Session2.member_id==Member.member_id).join(Role,Session2.role_id==Role.role_id).join(Role_Type,Role.role_type_id==Role_Type.role_type_id).join(Club,Member.club_id==Club.club_id).filter(Member.member_desc == member_).all()
-    dicts = []
-    for row in result:
-        d = {}
-        print(row.Session.session_dt, row.Role.role_desc, row.Role.role_desc, row.Role_Type.role_type_desc, row.Club.club_desc)
-        d['session_dt'] = row.Session.session_dt
-        d['member_desc'] = row.Role.role_desc
-        d['role_desc'] = row.Role.role_desc
-        d['role_type_desc'] = row.Role_Type.role_type_desc
-        d['club_desc'] = row.Club.club_desc
-        dicts.append(d)
-    
     results = pd.DataFrame(dicts)
-    results = results.groupby(by=["session_dt","member_desc","role_desc"]).agg({'role_type_id':'count'})
-    results = results.rename(columns={'role_type_id':'NoParticipations'})
     return results
 
-def getResultsPerRole(role_):
-    engine = create_engine(connection)
+
+def getResultsPerMember(_member,_year, _engine):
+    rs = _engine.execute(f'Select  m.member_desc,  (Extract(Year from session_dt)) as Anio,  (Extract(Month from session_dt)) as Mes,  rt.role_type_desc,  count(*) as NoPart  from public."Session" s  JOIN public."Member" m  ON m.member_id = s.member_id  JOIN public."Role" r  ON r.role_id = s.role_id  JOIN public."Role_Type" rt  ON r.role_type_id = rt.role_type_id  where m.member_desc like {_member}  and (Extract(Year from session_dt)) = {_year}  and "isGuest" = false  group by (Extract(Year from session_dt)),  (Extract(Month from session_dt)),  rt.role_type_desc,  m.member_desc ')
+    _engine.dispose()
+    dicts = []
+    for row in rs:
+        d = {}
+        d['member_desc'] = row[0]
+        d['year'] = row[1]
+        d['month'] = row[2]
+        d['role_type_desc'] = row[3]
+        d['nopart'] = row[4]
+        dicts.append(d)
+    results = pd.DataFrame(dicts)
+    return results
+
+def getDetailedResultsPerMember(_member,_year, _engine):
+    rs = _engine.execute(f'Select  m.member_desc, (Extract(Year from session_dt)) as Anio, (Extract(Month from session_dt)) as Mes, r.role_desc, rt.role_type_desc, count(*) as NoPart from public."Session" s JOIN public."Member" m ON m.member_id = s.member_id JOIN public."Role" r ON r.role_id = s.role_id JOIN public."Role_Type" rt ON r.role_type_id = rt.role_type_id where m.member_desc like {_member} and (Extract(Year from session_dt)) = {_year} group by (Extract(Year from session_dt)), (Extract(Month from session_dt)), r.role_desc, rt.role_type_desc, m.member_desc')
+    _engine.dispose()
+    dicts = []
+    for row in rs:
+        d = {}
+        d['member_desc'] = row[0]
+        d['year'] = row[1]
+        d['month'] = row[2]
+        d['role_type'] = row[3]
+        d['role_type_desc'] = row[4]
+        d['nopart'] = row[5]
+        dicts.append(d)
+    results = pd.DataFrame(dicts)
+    return results
+
+
+def getResultsPerRole(role_,_session, _engine):
+    engine = _engine
 
     # reflect an existing database into a new model
     Base = automap_base()
@@ -141,7 +86,7 @@ def getResultsPerRole(role_):
     Session_Type = Base.classes.Session_Type
 
     # Start session
-    Session = sessionmaker()
+    Session = _session
     Session.configure(bind=engine)
     session = Session()
 
@@ -166,15 +111,15 @@ def getResultsPerRole(role_):
         d['club_desc'] = row.Club.club_desc
         dicts.append(d)
     
-
+    session.close()
     results = pd.DataFrame(dicts)
     results = results.groupby(by=["role_desc"]).agg({'session_dt':'count'})
     results = results.rename(columns={'session_dt':'NoParticipations'})
     
     return results
 
-def getResultsPerRoleType(role_type_):
-    engine = create_engine(connection)
+def getResultsPerRoleType(role_type_, _session, _engine):
+    engine = _engine
 
     # reflect an existing database into a new model
     Base = automap_base()
@@ -192,7 +137,7 @@ def getResultsPerRoleType(role_type_):
     Session_Type = Base.classes.Session_Type
 
     # Start session
-    Session = sessionmaker()
+    Session = _session
     Session.configure(bind=engine)
     session = Session()
 
@@ -216,15 +161,15 @@ def getResultsPerRoleType(role_type_):
         d['role_type_desc'] = row.Role_Type.role_type_desc
         d['club_desc'] = row.Club.club_desc
         dicts.append(d)
-    
+    session.close()
     results = pd.DataFrame(dicts)
     results = results.groupby(by=["role_type_desc"]).agg({'session_dt':'count'})
     results = results.rename(columns={'session_dt':'NoParticipations'})
     
     return results
 
-def getResultsPerSession(date_):
-    engine = create_engine(connection)
+def getResultsPerSession(date_,_session, _engine):
+    engine = _engine
 
     # reflect an existing database into a new model
     Base = automap_base()
@@ -242,7 +187,7 @@ def getResultsPerSession(date_):
     Session_Type = Base.classes.Session_Type
 
     # Start session
-    Session = sessionmaker()
+    Session = _session
     Session.configure(bind=engine)
     session = Session()
 
@@ -270,17 +215,17 @@ def getResultsPerSession(date_):
     results = pd.DataFrame(dicts)
     results = results.groupby(by=["session_dt",""]).agg({'role_type_desc':'count'})
     results = results.rename(columns={'role_type_desc':'NoParticipations'})
-    
+    session.close()
     return results
 
-def getResultsPerDateRange(year_, month_):
+def getResultsPerDateRange(year_, month_, _session,_engine):
     
-    engine = create_engine(connection,client_encoding='utf8')
+    #engine = create_engine(connection,client_encoding='utf8')
 
     # reflect an existing database into a new model
     Base = automap_base()
     # reflect the tables
-    Base.prepare(engine, reflect=True)
+    Base.prepare(_engine, reflect=True)
 
     # Save references to each table
     Member = Base.classes.Member
@@ -290,18 +235,11 @@ def getResultsPerDateRange(year_, month_):
     Session2 = Base.classes.Session
     Session_Type = Base.classes.Session_Type
     # Start session
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-    session = Session()
+    _session.configure(bind=_engine)
     
-    # Read Tables to determine what task to perform in each
-    member_data = pd.read_sql_table("Member",engine)
-    club_data = pd.read_sql_table("Club",engine)
-    role_data = pd.read_sql_table("Role",engine)
-    role_type_data = pd.read_sql_table("Role_Type",engine)
-    session_data = pd.read_sql_table("Session",engine)
-    session_type_data = pd.read_sql_table("Session_Type",engine)
-    engine.dispose()
+    session = _session()
+    
+    _engine.dispose()
 
     num_days = calendar.monthrange(year_, month_)[1]
     start_date = datetime.date(year_, month_, 1)
@@ -325,14 +263,13 @@ def getResultsPerDateRange(year_, month_):
     results.reset_index(inplace=True)
     results.rename(columns={'member_desc':'Socios'}, inplace=True)
     #print(results.head())
+    session.close()
     return results
 
 
-def getStatsPerDateRange(year_, month_):
-    
-    engine = create_engine(connection,client_encoding='utf8')
-    
-    rs = engine.execute(f'Select extract(month from session_dt) as Month_Desc, count(*) as Participations, count(distinct member_id) as Members, count(distinct session_dt) as Sessions from public."Session" where extract(month from session_dt) = {month_} and extract(year from session_dt) = {year_} group by extract(month from session_dt)')
+def getStatsPerDateRange(year_, month_, _engine):
+    rs = _engine.execute(f'Select extract(month from session_dt) as Month_Desc, count(*) as Participations, count(distinct member_id) as Members, count(distinct session_dt) as Sessions from public."Session" where extract(month from session_dt) = {month_} and extract(year from session_dt) = {year_} group by extract(month from session_dt)')
+    _engine.dispose()
     dicts = []
     for row in rs:
         d = {}
@@ -344,11 +281,9 @@ def getStatsPerDateRange(year_, month_):
     results = pd.DataFrame(dicts)
     return results
 
-def getStatsPerYear(year_):
-    
-    engine = create_engine(connection,client_encoding='utf8')    
-    
-    rs = engine.execute(f'Select m.member_id, m.member_desc, rt.role_type_desc, count(*) as NoParticipations FROM public."Session" s JOIN public."Member" m On m.member_id = s.member_id JOIN public."Role" r On s.role_id = r.role_id JOIN public."Role_Type" rt On r.role_type_id = rt.role_type_id where extract(year from (session_dt)) = {year_} group by m.member_id, m.member_desc, rt.role_type_desc order by m.member_id, count(*) desc ')
+def getStatsPerYear(year_,_engine):
+    rs = _engine.execute(f'Select m.member_id, m.member_desc, rt.role_type_desc, count(*) as NoParticipations FROM public."Session" s JOIN public."Member" m On m.member_id = s.member_id JOIN public."Role" r On s.role_id = r.role_id JOIN public."Role_Type" rt On r.role_type_id = rt.role_type_id where extract(year from (session_dt)) = {year_} group by m.member_id, m.member_desc, rt.role_type_desc order by m.member_id, count(*) desc ')
+    _engine.dispose()
     dicts = []
     for row in rs:
         d = {}
@@ -357,6 +292,7 @@ def getStatsPerYear(year_):
         d['role_type_desc'] = row[2]
         d['NoParticipations'] = row[3]
         dicts.append(d)
+
     results = pd.DataFrame(dicts)
     results = results.pivot_table(values='NoParticipations',index=[results.member_desc],columns='role_type_desc',aggfunc='sum', dropna=True)
     results['Total'] = results.sum(axis=1)
@@ -374,10 +310,9 @@ def getStatsPerYear(year_):
     return results
 
 
-def getStatsPerClub(year_):
-    
-    engine = create_engine(connection,client_encoding='utf8')
-    rs = engine.execute(f'SELECT c.club_desc, count(distinct m.member_id) FROM public."Session" s JOIN public."Member" m 	ON m.member_id = s.member_id JOIN public."Club" c 	ON m.club_id = c.club_id where extract(year from (session_dt)) = {year_} group by c.club_desc order by count(distinct m.member_id) desc')
+def getStatsPerClub(year_,_engine):
+    rs = _engine.execute(f'SELECT c.club_desc, count(distinct m.member_id) FROM public."Session" s JOIN public."Member" m 	ON m.member_id = s.member_id JOIN public."Club" c 	ON m.club_id = c.club_id where extract(year from (session_dt)) = {year_} group by c.club_desc order by count(distinct m.member_id) desc')
+    _engine.dispose()
     dicts = []
     for row in rs:
         d = {}
@@ -392,10 +327,9 @@ def getStatsPerClub(year_):
     results.rename(columns={'club_desc':'Club'}, inplace=True)
     return results
 
-def getStatsPerSessionType(year_):
-    
-    engine = create_engine(connection,client_encoding='utf8')
-    rs = engine.execute(f'Select distinct session_type_desc, count(distinct session_dt)  from public."Session" s  JOIN public."Session_Type" st  	on st.session_type_id = s.session_type_id  WHERE EXTRACT(YEAR FROM (session_dt)) = {year_}  group by session_type_desc  order by count(distinct session_dt) desc')
+def getStatsPerSessionType(year_,_engine):
+    rs = _engine.execute(f'Select distinct session_type_desc, count(distinct session_dt)  from public."Session" s  JOIN public."Session_Type" st  	on st.session_type_id = s.session_type_id  WHERE EXTRACT(YEAR FROM (session_dt)) = {year_}  group by session_type_desc  order by count(distinct session_dt) desc')
+    _engine.dispose()
     dicts = []
     for row in rs:
         d = {}
--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import render_template
 from flask import redirect
 from flask import jsonify
 from flask import request
-from flask import current_app as app
+#from flask import current_app as app
 import pandas as pd
 import json
 import api_db
@@ -16,12 +16,13 @@ import api_db
 import pandas as pd
 import numpy as np
 
-import sqlalchemy
+#import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine, func, inspect, and_
+from credentials import Config
+#from sqlalchemy.ext.automap import automap_base
+#from sqlalchemy.orm import Session
+#from sqlalchemy.orm import sessionmaker
+#from sqlalchemy import create_engine, func, inspect, and_
 
 
 import os
@@ -32,24 +33,11 @@ import calendar
 #################################################
 # DB Connection
 #################################################
-db = SQLAlchemy()
-
-def create_app():
-    # Constructs the core aplication
-    app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('credentials.Config')
-    db.init_app(app)
-    
-    # Import routes and 
-    with app.app_context():
-        from . import Routes
-        db.create_all()
-        return app
-
-//https://hackersandslackers.com/flask-sqlalchemy-database-models/
-
-
-
+app = Flask(__name__, instance_relative_config=False)
+app.config.from_object('credentials.Config')
+db = SQLAlchemy(app)
+#db.get_engine(bind=Config.SQLALCHEMY_DATABASE_URI)
+print(Config.SQLALCHEMY_DATABASE_URI)
 
 #################################################
 # Flask Routes
@@ -73,31 +61,49 @@ def overview():
 #################################################
 @app.route("/api/v1/getResultsPerDateRange/<year_>/<month_>",methods=["GET"])
 def resultsPerDateRange(year_,month_):
-    json_results = api_db.getResultsPerDateRange(int(year_),int(month_))
+    json_results = api_db.getResultsPerDateRange(int(year_),int(month_),db.session.session_factory,db.get_engine())
     json_results = json_results.to_json()
     return jsonify(json_results)
 
 @app.route("/api/v1/getStatsPerDateRange/<year_>/<month_>",methods=["GET"])
 def statisticsPerDateRange(year_,month_):
-    json_results = api_db.getStatsPerDateRange(int(year_),int(month_))
+    json_results = api_db.getStatsPerDateRange(int(year_),int(month_),db.get_engine())
     json_results = json_results.to_json()
     return jsonify(json_results)
 
 @app.route("/api/v1/getStatsPerYear/<year_>",methods=["GET"])
 def statisticsPerYear(year_):
-    json_results = api_db.getStatsPerYear(int(year_))
+    json_results = api_db.getStatsPerYear(int(year_),db.get_engine())
     json_results = json_results.to_json()
     return jsonify(json_results)
 
 @app.route("/api/v1/getStatsPerClub/<year_>",methods=["GET"])
 def statisticsPerClub(year_):
-    json_results = api_db.getStatsPerClub(int(year_))
+    json_results = api_db.getStatsPerClub(int(year_),db.get_engine())
     json_results = json_results.to_json()
     return jsonify(json_results)
 
 @app.route("/api/v1/getStatsPerSessionType/<year_>",methods=["GET"])
 def statisticsPerSessionType(year_):
-    json_results = api_db.getStatsPerSessionType(int(year_))
+    json_results = api_db.getStatsPerSessionType(int(year_),db.get_engine())
+    json_results = json_results.to_json()
+    return jsonify(json_results)
+
+@app.route("/api/v1/getAllResults/<member_>/<year_>",methods=["GET"])
+def resultsAllResults(member_,year_):
+    json_results = api_db.getAllResults(member_,int(year_),db.session.session_factory,db.get_engine())
+    json_results = json_results.to_json()
+    return jsonify(json_results)
+
+@app.route("/api/v1/getRoleResults/<member_>/<year_>",methods=["GET"])
+def resultsRoleResults(member_,year_):
+    json_results = api_db.getResultsPerMember(member_,int(year_),db.session.session_factory,db.get_engine())
+    json_results = json_results.to_json()
+    return jsonify(json_results)
+
+@app.route("/api/v1/getDetailedResults/<member_>/<year_>",methods=["GET"])
+def resultsDetailedResults(member_,year_):
+    json_results = api_db.getDetailedResultsPerMember(member_,int(year_),db.session.session_factory,db.get_engine())
     json_results = json_results.to_json()
     return jsonify(json_results)
 
