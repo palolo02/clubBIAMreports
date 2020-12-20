4b5a57f6c6e0a6f94a1b0ab6d8e67c5c2087236c
Paolo Vega <pao02esc@gmail.com>
2020-12-19 23:24:37 -0600

    Update in req packages. Add models for authentication

---
+++ app/models.py
@@ -0,0 +1,44 @@
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from . import db
+
+
+class UserRole(db.Model):
+    __tablename__ = 'UserRole'
+    user_role_id = db.Column(db.Integer, primary_key=True)
+    user_role_desc = db.Column(db.String(64), unique=True, index=True)
+    is_active = db.Column(db.Boolean)
+    permission = db.Column(db.SmallInteger)
+    start_dt = db.Column(db.Date)
+    update_dt = db.Column(db.Date)
+
+    def __repr__(self):
+        return '<UserRole %r>' % self.name
+
+
+
+class User(db.Model):
+    __tablename__ = 'User'
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_desc = db.Column(db.String(64), unique=True, index=True)
+    name = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    user_role_id = db.Column(db.SmallInteger, db.ForeignKey('UserRole.user_role_id'))    
+    is_active = db.Column(db.Boolean)
+    start_dt = db.Column(db.Date)
+    update_dt = db.Column(db.Date)
+    
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
